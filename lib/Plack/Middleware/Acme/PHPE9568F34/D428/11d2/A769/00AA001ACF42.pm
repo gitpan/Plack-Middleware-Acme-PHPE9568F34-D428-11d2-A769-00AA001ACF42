648b06342c4a2609224431d599a9e6343bc8afcd
Arthur Axel "fREW" Schmidt <frioux+cpan@gmail.com>
2011-05-28 02:43:46 +0000

    Import of FREW/Plack-Middleware-Acme-PHPE9568F34-D428-11d2-A769-00AA001ACF42-3.000100 from CPAN.
    gitpan-cpan-distribution: Plack-Middleware-Acme-PHPE9568F34-D428-11d2-A769-00AA001ACF42
    gitpan-cpan-version:      3.000100
    gitpan-cpan-path:         FREW/Plack-Middleware-Acme-PHPE9568F34-D428-11d2-A769-00AA001ACF42-3.000100.tar.gz
    gitpan-cpan-author:       FREW
    gitpan-cpan-maturity:     released

--- lib/Plack/Middleware/Acme/PHPE9568F34/D428/11d2/A769/00AA001ACF42.pm
+++ lib/Plack/Middleware/Acme/PHPE9568F34/D428/11d2/A769/00AA001ACF42.pm
@@ -1,6 +1,6 @@
 package Plack::Middleware::Acme::PHPE9568F34::D428::11d2::A769::00AA001ACF42;
 BEGIN {
-  $Plack::Middleware::Acme::PHPE9568F34::D428::11d2::A769::00AA001ACF42::VERSION = '3.000000';
+  $Plack::Middleware::Acme::PHPE9568F34::D428::11d2::A769::00AA001ACF42::VERSION = '3.000100';
 }
 
 # ABSTRACT: Implement PHP's best features with PSGI!
@@ -61,7 +61,7 @@ Plack::Middleware::Acme::PHPE9568F34::D428::11d2::A769::00AA001ACF42 - Implement
 
 =head1 VERSION
 
-version 3.000000
+version 3.000100
 
 =head1 SYNOPSIS
 
